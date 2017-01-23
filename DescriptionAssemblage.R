5ca7fe552dc07a504f828379f0ec6f174fedbe9c
Pierre Charrier <pcharrier@template>
2017-01-23 22:49:50 +0100

    Adding the first version of DescriptionAssemblage.R in a first commit in my first git repo :) This French-style R script produces a tabular report of some assembly statistics from an assembly (median, N50, mean, shortest contig, longest contig, ...) This script needs a tabular formated input : ContigName (\t) size

---
+++ DescriptionAssemblage.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+library("optparse")
+ 
+option_list = list(
+  make_option(c("-f", "--file"), type="character", default=NULL, 
+              help="dataset file name", metavar="character"),
+	make_option(c("-o", "--out"), type="character", default=NULL, 
+              help="output file name", metavar="character")
+); 
+ 
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$file) | is.null(opt$out)){
+  print_help(opt_parser)
+  stop("Two arguments must be supplied (input and output file name).n", call.=FALSE)
+}
+
+## program...
+df = read.table(opt$file, header=TRUE)
+num_vars = which(sapply(df, class)=="numeric")
+df_out = df[ ,num_vars]
+write.table(df_out, file=opt$out, row.names=FALSE)
+
+
+data = read.table(opt$file, h=F)
+names(data)=c("Seq", "length")
+
+sortedLength = rev(sort(data$length))
+NumberOfContigs = length(data$length)
+medianAssembly = median(data$length)
+meanAssembly = round(mean(data$length))
+shortest = min(data$length)
+longest = max(data$length)
+totalsize = sum(data$length)
+Sup500 = length(data[data$length>500,1])
+Sup1k = length(data[data$length>1000,1])
+Sup10k = length(data[data$length>10000,1])
+N50 = sortedLength[min(which(cumsum(sortedLength)>sum(sortedLength)/2))] 
+res = c(NumberOfContigs, totalsize, shortest, longest, Sup500, Sup1k, Sup10k, meanAssembly, medianAssembly, N50)
+RowNames = c("Number of contigs", "total size of contigs", "Shortest contigs", "Longest contigs", "Number of contigs > 500", "Number of contigs > 1k", "Number of contig > 10k", "mean contig size", "median contig size","N50 contig length")
+write.table(cbind(RowNames, res), file = opt$out, quote=F, row.names=F, sep="\t")
